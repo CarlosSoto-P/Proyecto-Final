41657b861ea65a1d044fb6fbb1e48f195eec26a5
CarlosSoto-P <carlos.soto2000@hotmail.com>
2020-11-21 18:23:38 -0700

    first commit

---
+++ includes/header.php
@@ -0,0 +1,49 @@
+<div class="sticky-top">
+
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+            <img src="imagenes/logo.jpeg" width="60" height="60" alt="">
+        </a>
+        <a class="navbar-brand" href="#">Let's Teach</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Grupos</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Amigos</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Mensajes</a>
+                </li>
+
+
+
+
+
+            </ul>
+
+
+
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="¿Que buscas?" aria-label="Search">
+                <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Buscar</button>
+            </form>
+
+
+
+
+
+
+        </div>
+    </nav>
+</div>
