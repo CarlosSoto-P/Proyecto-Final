2ab8077e926d2b1070432ee910c46993911c8030
Sebastian-Santana <71776887+Sebastian-Santana@users.noreply.github.com>
2020-11-27 15:02:49 -0700

    hola

--- includes/barraLateralGrupo.php
+++ includes/barraLateralGrupo.php
@@ -2,7 +2,7 @@
                     <div class="card gedf-card">
                         <div class="card-body">
                             <h3 class="card-title">Nombre del grupo</h3>
-                            <a href="editarPerfil.php" class="card-link">Abandonar grupo</a>
+                            <a href="#" class="card-link">Abandonar grupo</a>
                             <a href="informacionGrupo.php"   class="card-link">Informacion del Grupo</a>
                         </div>
                     </div>
--- informacionGrupo.php
+++ informacionGrupo.php
@@ -1,17 +1,52 @@
 
 <?php
+
+include("connections/conn_localhost.php");
+
+// Inicializamos la sesion o la retomamos
+if(!isset($_SESSION)) {
+  session_start();
+}
+if(!isset($_SESSION['id'])) header('Location: login.php');
+
+$query_userData = sprintf("SELECT * FROM usuario WHERE idUsuario =%d",
+mysqli_real_escape_string($connLocalhost, trim($_SESSION['id']))
+);
+
+$resQueryUserData = mysqli_query($connLocalhost, $query_userData) or trigger_error("El query para obtener los detalles del usuario loggeado falló");
+
+$userData= mysqli_fetch_assoc($resQueryUserData);
+
+// Incluimos la conexión a la base de datos
 include("connections/conn_localhost.php");
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Informacion del grupo</title>
 </head>
 <body>
+
+    <main>
+
+        <section id="cabecera">
+            <header>
+                <?php include("includes/header.php"); ?>
+            </header>
+        </section>
+           
+
+        <section id = >
+        
+        </section>
+
+    </main>
     
 </body>
 </html>
