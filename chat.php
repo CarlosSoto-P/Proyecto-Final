c5ea42ac7c2a1bbe3e96a131e6b0342f73b353c9
beto519 <74939645+beto519@users.noreply.github.com>
2020-11-27 18:08:38 -0700

    Create chat.php

---
+++ chat.php
@@ -0,0 +1,13 @@
+
+ <?php
+ include "connections/conn_localhost.php";
+    $consulta = "SELECT * FROM byvwn65uxblxcrtg2waf.message ;";
+    $resQueryMessage = mysqli_query($connLocalhost, $consulta) or trigger_error("El query falló");
+        while ($fila = $resQueryMessage->fetch_array()): 
+?>
+    <div id="data-chat">
+            <span style="color:#1c62c4;"><?php echo $fila['nombre'] ?>: </span>
+             <span style="color:#848484;"><?php echo $fila['mensaje'] ?></span>
+             <span style="float:right;"><?php echo formatfecha($fila['fecha']) ?></span>
+    </div>
+<?php endwhile;?>
