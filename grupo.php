1e31992128f62c6321f0708cd162ff1056f158eb
beto519 <74939645+beto519@users.noreply.github.com>
2020-12-10 00:40:51 -0700

    Update grupo.php

--- grupo.php
+++ grupo.php
@@ -38,26 +38,22 @@ include("connections/conn_localhost.php");
 <!DOCTYPE html>
 <html lang="es">
 <head>
+
+
+<script type="text/javascript" src="js/likes.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+    <script type="text/javascript" src="js/likes.js"></script>
     <title>Grupo</title>
 </head>
 <body>
 
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
-        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
-    </script>
+   
 
     <main>
 
@@ -111,6 +107,7 @@ include("connections/conn_localhost.php");
                             </div>
                             <div clas="h5">
                             
+                            
                                 
                                     <?php 
                                         //while para mostrar todos los miembros 
@@ -142,6 +139,7 @@ include("connections/conn_localhost.php");
                 </div>
 
 
+
                 <div class="col-md-6 gedf-main">
 
               
@@ -164,20 +162,17 @@ include("connections/conn_localhost.php");
                         </div>
                         <div class="card-body">
                             <div class="tab-content" id="myTabContent">
-                           <!-- Redordatorio no poner action.-->
-                            <form  method="post">
                                 <div class="tab-pane fade show active" id="posts" role="tabpanel"
-                                
                                     aria-labelledby="posts-tab">
                                     <div class="form-group">
-                                   
+                             <form  method="post">
                                         <label class="sr-only" for="message">post</label>
-                                       
-                                        <input  class="form-control publicar" id = 'title' name = 'title' placeholder="Titulo" rows="3" type="text"></input>
-                                        <textarea name = 'datosmsg'  class="form-control" id="datosmsg"  rows="3"
+
+                                        <input  class="form-control publicar" id ='publicar' name = 'title' placeholder="Titulo" rows="3" type="text"></input>
+                                        <textarea name = 'datosmsg'  class="form-control" id="message"  rows="3"
                                             placeholder="¿Cual es tu duda?"></textarea>
                                     </div>
-                               
+
                                 </div>
 
                             </div>
@@ -185,18 +180,15 @@ include("connections/conn_localhost.php");
 
                             <div class="btn-toolbar justify-content-between">
                                 <div class="btn-group">
-                                
-                                   <button  id="publicar" name="publicar">Publicar</button>
-                                
-                                   
-                                </div>
-                                </form>
+                                <button  id="btnPublicar" name="btnPublicar">Publicar</button>
+                                     </div>
 
                             </div>
                         </div>
 
 
                     </div>
+                    
 
                     <!--- \\\\\\\publicaciones-->
 
@@ -206,16 +198,11 @@ include("connections/conn_localhost.php");
  $usuarioId = "";
  $boxText = "";
  $boxTitle = "";
- 
+
  if (isset($_POST['btnPublicar'])){
  
 
-    $grupoID = "";
-    $usuarioId = "";
-    $boxText = "";
-    $boxTitle = "";
-
-    if (isset($_POST['publicar'])) {
+  
     $grupoID = $grupoData['idGrupo'];
     $usuarioId = $userData['idUsuario'];
     $boxText = $_POST['datosmsg'];
@@ -223,7 +210,7 @@ include("connections/conn_localhost.php");
     $mg = 0;
     $consulta = sprintf("INSERT INTO publicacion (idGrupo,idUsuario,contenido,titulo,megustas) VALUES ('%s','%s','%s','%s','%s')",
     mysqli_real_escape_string($connLocalhost, trim($grupoID)),
-    mysqli_real_escape_string($connLocalhost, trim($grupoID)),
+    mysqli_real_escape_string($connLocalhost, trim($usuarioId)),
     mysqli_real_escape_string($connLocalhost, trim($boxText)),
     mysqli_real_escape_string($connLocalhost, trim($boxTitle)),
     mysqli_real_escape_string($connLocalhost, trim($mg))
@@ -232,11 +219,11 @@ include("connections/conn_localhost.php");
 );
 $resQueryMessage = mysqli_query($connLocalhost, $consulta) or trigger_error("El query falló");
 
+    
  }
- 
 
 
-                            for ($i=0; $i <100; $i++):
+                          
                             ?>
 
                    
@@ -244,9 +231,13 @@ $resQueryMessage = mysqli_query($connLocalhost, $consulta) or trigger_error("El 
 
                     <?php
 
+                    
+                        //query para sacar las publicaciones de la base de datos
+                            
+                    if(1>0){
 
 
-                        $ids = $grupoData['idGrupos'];
+                        $ids = $grupoData['idGrupo'];
                         
                         $query_publicaciones = ("SELECT 
                         usuario.idUsuario as 'idUsuario',
@@ -270,6 +261,7 @@ $resQueryMessage = mysqli_query($connLocalhost, $consulta) or trigger_error("El 
                             do{
                             ?>
 
+                                         
                     <div class="card gedf-card">
                         <div class="card-header">
                             <div class="d-flex justify-content-between align-items-center">
@@ -280,36 +272,79 @@ $resQueryMessage = mysqli_query($connLocalhost, $consulta) or trigger_error("El 
                                     <div class="ml-2">
                                         <div class="h5 m-0">
 
-                                            <a href="perfil.php">@nombreUsuarioMiembro
-                                            <?php echo($i)?>
+                                            <a href="perfil.php?idUsuario=<?php echo $publicaciones['idUsuario']?>">
+                                                <span class="text-primary"> <?php echo($publicaciones['nombre'])?>
+                                                </span>
                                             </a>
                                         </div>
-                                        <div class="h7 text-muted">nombreCompletoMiembro</div>
-                                        <a class="text-muted" href="#">>nombreGrupo</a>
+                                        <div class="h7 text-muted"><?php echo($publicaciones['apellido'])?></div>
+                                        <a class="text-dark" href="grupo.php?idGrupo=<?php echo $publicaciones['idGrupo']; ?>">><?php echo($publicaciones['grupo'])?></a>
                                     </div>
                                 </div>
                                 <div>
-                                    
+
                                 </div>
                             </div>
 
                         </div>
                         <div class="card-body">
-                            <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i>10 min ago</div>
 
-                            <h5 class="text-primary">Lorem ipsum dolor sit amet, consectetur adip.</h5>
+                            <h5 class="text-info"><?php echo($publicaciones['titulo'])?></h5>
 
                             <p class="card-text">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem
-                                eos
-                                ipsa praesentium esse magnam nemo dolor
-                                sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.
+                                <?php echo($publicaciones['contenido'])?>
                             </p>
                         </div>
-                        <div class="card-footer">
-                            <a href="#" class="card-link"><i class="fa fa-gittip"></i> Like</a>
-                            <!--<a href="#" class="card-link"><i class="fa fa-comment"></i> Comment</a> -->
 
+
+
+
+                        <div id="publicaciones" class="card-footer">
+
+
+
+                            <?php 
+                            $query_megusta = sprintf("SELECT * FROM megustas WHERE idUsuario =%d AND idPublicacion = %d",
+                            mysqli_real_escape_string($connLocalhost, trim($userData['idUsuario'])),
+                            mysqli_real_escape_string($connLocalhost, trim($publicaciones['idPublicacion'])));
+
+                            $resquery_query_megusta = mysqli_query($connLocalhost,$query_megusta) or trigger_error(" la query de megustas fallo");
+                            
+                            
+                            if(mysqli_num_rows($resquery_query_megusta)==0){?>
+                              <span class="like text-info"
+                                id="cantidad_<?php echo $publicaciones['idPublicacion'] ?>"><?php echo $publicaciones['megustas']?></span>
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill"
+                                fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+                            </svg>
+                          
+                            <a style="cursor:pointer" class="card-link"><i class="fa fa-gittip"></i><span class="like"
+                                    id="<?php echo $publicaciones['idPublicacion'] ?>">Me gusta</span></a>
+
+
+
+
+                            <?php } else {?>
+                                <span class="like text-info"
+                                id="cantidad_<?php echo $publicaciones['idPublicacion'] ?>"><?php echo $publicaciones['megustas']?></span>
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill"
+                                fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+                            </svg>
+                            
+                            <a style="cursor:pointer" class="card-link"><i class="fa fa-gittip"></i><span class="like"
+                                    id="<?php echo $publicaciones['idPublicacion'] ?>">No me gusta</span></a>
+
+
+
+                            <?php } ?>
+
+
+
+                            <!---<a href="#" class="card-link"><i class="fa fa-comment"></i> Comment</a>--->
                         </div>
                     </div>
                     <hr style="height:2px;border-width:0;color:gray;background-color:gray">
@@ -327,6 +362,7 @@ $resQueryMessage = mysqli_query($connLocalhost, $consulta) or trigger_error("El 
                     <?php
                     }
                     ?>
+                    
 
                                     </div>
                                 </div>
@@ -340,14 +376,15 @@ $resQueryMessage = mysqli_query($connLocalhost, $consulta) or trigger_error("El 
                     <br>
                     <hr>
                     <br>
-                    <?php endfor; ?>
+                
                     <!-- Post /////-->
+                    
                 </div>
 
+ <!-- barra lateral -->
+</div>
 
-                <!-- barra lateral -->
-
-                <div class="col-md-3 float-right">
+            <div class="col-md-3 float-right">
                     <div class="card gedf-card">
                         <div class="card-body">
                             <h3 class="card-title">
@@ -378,18 +415,15 @@ $resQueryMessage = mysqli_query($connLocalhost, $consulta) or trigger_error("El 
                     </div>
                    
                 </div>
-                
+               
 
             </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
-        </script>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-        </script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
             integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+            integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
         </script>
 
 
