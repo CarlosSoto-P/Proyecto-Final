75e57c96195709bb397ce033434385ffaf43293a
CarlosSoto-P <carlos.soto2000@hotmail.com>
2020-12-10 21:57:04 -0700

    SHT

--- megusta.php
+++ megusta.php
@@ -4,7 +4,7 @@ include("connections/conn_localhost.php");
 $post = $_POST['id'];
 
 $usuario = $_SESSION['id'];
-$query_megusta = sprintf("SELECT * FROM megustas WHERE idUsuario =%d AND idPublicacion = %d",
+$query_megusta = sprintf("SELECT * FROM SHT_megustas WHERE idUsuario =%d AND idPublicacion = %d",
 mysqli_real_escape_string($connLocalhost, trim($usuario)),
 mysqli_real_escape_string($connLocalhost, trim($post)));
 
@@ -15,26 +15,26 @@ $count = mysqli_num_rows($resquery_query_megusta);
 
 if($count ==0){
 
-$insert_megusta=("INSERT INTO  megustas (idUsuario,idPublicacion) VALUES ($usuario,$post)");
+$insert_megusta=("INSERT INTO  SHT_megustas (idUsuario,idPublicacion) VALUES ($usuario,$post)");
 
 
 $res_inserMegusta = mysqli_query($connLocalhost,$insert_megusta) or trigger_error("fallo megusta");
 
-$update_publicacion = ("UPDATE publicacion SET megustas = megustas+1 WHERE idPublicacion = $post");
+$update_publicacion = ("UPDATE SHT_publicacion SET megustas = megustas+1 WHERE idPublicacion = $post");
 $res_update = mysqli_query($connLocalhost,$update_publicacion);
 
 
 
 }else{
-$delete_megusta = ("DELETE FROM megustas WHERE idPublicacion =$post AND idUsuario =$usuario");
+$delete_megusta = ("DELETE FROM SHT_megustas WHERE idPublicacion =$post AND idUsuario =$usuario");
 $res_delete = mysqli_query($connLocalhost, $delete_megusta);
-$update_publicacion = ("UPDATE publicacion SET megustas = megustas-1 WHERE idPublicacion = $post");
+$update_publicacion = ("UPDATE SHT_publicacion SET megustas = megustas-1 WHERE idPublicacion = $post");
 $res_update = mysqli_query($connLocalhost,$update_publicacion);
 
 }
 
 
-$contar = ("SELECT * FROM publicacion WHERE idPublicacion = $post");
+$contar = ("SELECT * FROM SHT_publicacion WHERE idPublicacion = $post");
 $res_contar = mysqli_query($connLocalhost, $contar);
 $cont = mysqli_fetch_assoc($res_contar);
 
